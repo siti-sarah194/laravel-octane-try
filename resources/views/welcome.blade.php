3b109c7ecd8c62e5829dc426dd2602d7fa06b9e8
fadlikhalid <58224360+fadlikhalid@users.noreply.github.com>
2022-03-30 18:09:01 +0800

    ayam

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,6 +51,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                    Ayam ayam ayam ayam
                                 </div>
                             </div>
                         </div>
