005f4eb487d27d409c1b8ad3fb933f886fbba0e6
siti-sarah194 <72914408+siti-sarah194@users.noreply.github.com>
2022-04-21 11:11:28 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Deploy Test 2
+                                    Hello!
                                 </div>
                             </div>
                         </div>
